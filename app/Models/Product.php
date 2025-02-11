83daa392ca481a859ee6a7f104234c5dff2307c5
Senesh Akmeemana <seneshak2001@gmail.com>
2025-02-11 20:55:03 +0530

    fix: updated model.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'barcode',
+        'price',
+        'stock',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
 }
