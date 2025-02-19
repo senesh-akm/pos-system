a7e1ad9d614082d32357e6bf5d283894120ea2e8
Senesh Akmeemana <seneshak2001@gmail.com>
2025-02-19 21:10:24 +0530

    test: added three new test functions for the product module test case.

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -43,5 +44,74 @@ class ProductTest extends TestCase
         ]);
 
         $response->assertSessionHasErrors(['name', 'barcode', 'price', 'stock']);
+    }
+
+    #[Test]
+    public function test_an_updates_can_be_product()
+    {
+        $this->withoutExceptionHandling();
+
+        $product = Product::create([
+            'name' => 'Old Product',
+            'barcode' => '123456789',
+            'price' => 500,
+            'stock' => 'In Stock',
+        ]);
+
+        $updateProduct = [
+            'name' => 'Replace Product',
+            'barcode' => '012345678',
+            'price' => 800,
+            'stock' => 'Out of Stock',
+        ];
+
+        $response = $this->put(route('products.update', $product->id), $updateProduct);
+        $response->assertRedirect(route('products.index'));
+
+        $this->assertDatabaseHas('products', [
+            'id' => $product->id,
+            'name' => 'Replace Product',
+            'barcode' => '012345678',
+            'price' => 800,
+            'stock' => 'Out of Stock',
+        ]);
+    }
+
+    #[Test]
+    public function test_validates_a_product_update_data()
+    {
+        $product = Product::create([
+            'name' => 'Test Product',
+            'barcode' => '987654321',
+            'price' => 600,
+            'stock' => 'In Stock',
+        ]);
+
+        $response = $this->put(route('products.update', $product->id), [
+            'name' => '',
+            'barcode' => '',
+            'price' => '',
+            'stock' => '',
+        ]);
+
+        $response->assertSessionHasErrors(['name', 'barcode', 'price', 'stock']);
+    }
+
+    #[Test]
+    public function test_deletes_a_product_data()
+    {
+        $this->withoutExceptionHandling();
+
+        $product = Product::create([
+            'name' => 'Delete Product',
+            'barcode' => '456789123',
+            'price' => 400,
+            'stock' => 'Out of Stock',
+        ]);
+
+        $response = $this->delete(route('products.destroy', $product->id));
+        $response->assertRedirect(route('products.index'));
+
+        $this->assertDatabaseMissing('products', ['id' => $product->id]);
     }
 }
