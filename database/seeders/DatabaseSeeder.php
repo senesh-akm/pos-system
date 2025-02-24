a2f31c249601490378ef5a60c203fdebf4c3135d
Senesh Akmeemana <seneshak2001@gmail.com>
2025-02-24 20:55:16 +0530

    chore: remove Laravel Spatie package and other configuration files.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,15 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = Role::create(['name' => 'admin']);
-        $cashier = Role::create(['name' => 'cashier']);
-
-        Permission::create(['name' => 'manage products']);
-        Permission::create(['name' => 'process sales']);
-
-        $admin->givePermissionTo(['manage products', 'process sales']);
-        $cashier->givePermissionTo('process sales');
-
-        Role::firstOrCreate(['name' => 'cashier']);
+        //
     }
 }
